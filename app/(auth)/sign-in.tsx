bf4a3614bf538e6901d7e849a9513ed217418f72
Chidiebere Onyegbuchulem <chidex5@gmail.com>
2024-10-16 02:56:51 +0100

    feat: restructure files and add new assets

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,14 @@
+import { Text } from 'react-native';
+import {
+  SafeAreaView
+} from 'react-native-safe-area-context';
+
+const SignIn = () => {
+  return (
+    <SafeAreaView>
+      <Text>sign in </Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,14 @@
+import { Text } from 'react-native';
+import {
+  SafeAreaView
+} from 'react-native-safe-area-context';
+
+const Rides = () => {
+  return (
+    <SafeAreaView>
+      <Text>rides</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Rides;
